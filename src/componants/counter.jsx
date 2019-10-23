471ff846f4efb421f61818c63981b12b0870423c
Saqib Shah <xaqibshah@gmail.com>
2019-10-23 13:13:49 +0500

    adding Counting Functionality Componant

--- src/componants/counter.jsx
+++ src/componants/counter.jsx
@@ -2,14 +2,44 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
+    count: 0,
+    tags: [],
   };
+  //   constructor() {
+  //     super();
+  //     this.handlerIncrement = this.handlerIncrement.bind(this);
+  //   }
+
+  handlerIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  //   renderTags() {
+  //     if (this.state.tags.length === 0) return <p> Create New Tags! </p>;
+
+  //     return (
+  //       <ul>
+  //         {" "}
+  //         {this.state.tags.map(tag => (
+  //           <li key={tag}>{tag}</li>
+  //         ))}{" "}
+  //       </ul>
+  //     );
+  //   }
 
   render() {
     return (
       <div>
+        {/* {this.state.tags.length === 0 && "Plsear Add new tags"}
+        {this.renderTags()} */}
+
         <span className={this.getBadgeClasses()}>{this.fomartCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handlerIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
